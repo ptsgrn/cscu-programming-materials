b3f04d927cb7d7cb728eee024f3b5518a1c2189e
Patsagorn <patsagorn_y@outlook.com>
2025-03-30 03:10:32 +0700

    feat(2301260/lab10): add files

---
+++ 2301260-prog-tech/lab10/src/BinarySearchTree.java
@@ -0,0 +1,38 @@
+public class BinarySearchTree {
+    private BTNode root;
+
+    public void add(int data) {
+        if (this.root == null) {
+            this.root = new BTNode(data);
+            return;
+        }
+        BTNode currentNode = this.root;
+        BTNode preNode = null;
+
+        while (currentNode != null) {
+            preNode = currentNode;
+            if (data < currentNode.getData()) {
+                currentNode = currentNode.getLeft();
+            } else {
+                currentNode = currentNode.getRight();
+            }
+        }
+
+        if (data < preNode.getData()) {
+            preNode.setLeft(new BTNode(data));
+        } else {
+            preNode.setRight(new BTNode(data));
+        }
+    }
+
+    public void printTree() {
+        inorder(root);
+    }
+
+    public void inorder(BTNode node) {
+        if (node == null) return;
+        inorder(node.getLeft());
+        System.out.print(node.getData() + "\t");
+        inorder(node.getRight());
+    }
+}
---
+++ 2301260-prog-tech/lab10/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(9);
+        tree.add(2);
+        tree.add(12);
+        tree.add(1);
+        tree.add(5);
+        tree.add(10);
+        tree.add(15);
+        tree.printTree();
+    }
+}
